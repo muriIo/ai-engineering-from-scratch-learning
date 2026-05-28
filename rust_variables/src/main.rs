8f5fee49c99a9e8ad4db0ab67245125627208846
Murilo Cruz <murilomecr@gmail.com>
2026-05-28 19:54:06 -0300

    adding static variables

--- rust_variables/src/main.rs
+++ rust_variables/src/main.rs
@@ -1,3 +1,6 @@
+const GLOBAL_CONST: i8 = 30;
+static FIRST_STATIC_VARIABLE: i8 = 21;
+
 fn main() {
     let x = 5;
 
@@ -16,4 +19,32 @@ fn main() {
     let spaces = spaces.len();
 
     println!("The length of spaces is {}", spaces);
+
+    println!("The global const can be use here at main: {}", GLOBAL_CONST);
+
+    println!(
+        "The static variable can be use here at main: {}",
+        FIRST_STATIC_VARIABLE
+    );
+
+    print_constants_and_statics();
+}
+
+fn print_constants_and_statics() {
+    println!(
+        "The global const can be use here at printConstantsAndStatics: {}",
+        GLOBAL_CONST
+    );
+
+    println!(
+        "The static variable can be use here at printConstantsAndStatics: {}",
+        FIRST_STATIC_VARIABLE
+    );
+
+    const LOCAL_CONST: i8 = 55;
+
+    println!(
+        "The local const can only be use here at printConstantsAndStatics: {}",
+        LOCAL_CONST
+    );
 }
