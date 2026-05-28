751fa2d781746c6a7bce237b0b6bf7d8bec46f85
Murilo Cruz <murilomecr@gmail.com>
2026-05-28 19:34:06 -0300

    declaring variables in rust -> immutability, mutability and shadowing

---
+++ rust_variables/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let x = 5;
+
+    println!("The value of x is: {}", x);
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    let spaces = "    ";
+
+    println!("Space as text: {} finished.", spaces);
+
+    let spaces = spaces.len();
+
+    println!("The length of spaces is {}", spaces);
+}
